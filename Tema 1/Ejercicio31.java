06b80ebf1cb2e99f9d77e3ffc82e45b83c31ccb3
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-10-11 09:57:25 +0200

    Ejercicio 31, mal hecho

---
+++ Tema 1/Ejercicio31.java
@@ -0,0 +1,14 @@
+public class Ejercicio31 {
+    public static void main(String[] args) {
+
+        int[] anArray = new int[10] , valor = 100 ;
+
+        for (int i= 0, 10>i, ++i){
+            anArray[i] = valor ;
+            valor = valor + 100 ;
+        }
+        for (int i=0, 10>1, ++i){
+            System.out.println(anArray[i]) ;
+        }
+    }
+}
