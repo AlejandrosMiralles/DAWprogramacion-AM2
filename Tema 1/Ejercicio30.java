7cfd02bf494d686e0c4658a0b2c5c2d6b7a731bb
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-10-13 09:13:18 +0200

    Ejercicio 33 icompleto

--- Tema 1/Ejercicio30.java
+++ Tema 1/Ejercicio30.java
@@ -1,24 +1,6 @@
 //Programa que descubre si una frase es palíndroma o no. (Usar lenght())
 
-import java.util.Scanner ;
-
 public class Ejercicio30 {
     public static void main(String[] args) {
-
-        String palindromo, inverso ;
-        public int lenght() ;
-        Scanner lector = new Scanner(System.in) ;
-        
-        System.out.println("Dame una palabra o una frase y te digo si es palíndroma o no:") ;
-        palindromo = lector.next() ;
-        lector.close() ;
-
-        int lenght = palindromo.lenght() ;
-
-        for (int i=0; i< palindromo.lenght(); ++i){
-
-        }
-
-
     }
 }
--- Tema 1/Ejercicio33.java
+++ Tema 1/Ejercicio33.java
@@ -1,5 +1,77 @@
 //Programa que coge un DNi sin número y calcula su letra
 
+import java.util.Scanner ;
+
 public class Ejercicio33 {
-    
+    public static void main(String[] args) {
+
+        int dni , resto ;
+        Scanner lector = new Scanner(System.in) ;
+
+
+        System.out.println("Dame un DNI y te daré la versión española del mismo:") ;
+        dni = lector.nextInt() ;
+
+        resto = dni%23 ;
+
+        switch (resto){
+            case 0: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 1: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 2: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 3: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 4: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 5: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 6: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 7: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 8: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 9: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 10: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 11: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 12: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 13: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 14: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 15: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 16: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            case 17: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;case 0: 
+                    System.out.println("El DNi es: "+dni+"R") ;
+                    break ;
+            }
+        }
+    }
 }
