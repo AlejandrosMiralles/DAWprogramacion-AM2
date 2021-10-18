f7adef73b6135582d66b3b22887ad2036955f155
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-10-18 09:41:25 +0200

    Ejercicio 40

--- DAWprogramacion-AM3/DAWprogramacion-AM4/Tema 1/Ejercicio40.java
+++ DAWprogramacion-AM3/DAWprogramacion-AM4/Tema 1/Ejercicio40.java
@@ -1,6 +1,6 @@
 public class Ejercicio40 {
     public static void main(String[] args) {
-        long n1= 1, n2=1, t ;
+        double n1= 1, n2=1, t ;
         byte i;
 
         System.out.print(n1+", "+n2);
@@ -9,7 +9,7 @@ public class Ejercicio40 {
 
         while (i<=40){
             t = n1+n2 ;
-            System.out.print(", "+t);
+            System.out.print(", "+t/n2);
 
             n1 = n2 ;
             n2 = t ;
