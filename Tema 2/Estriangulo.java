0e28722788f418929744383a7de09b958d69eb4a
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-10-27 10:43:14 +0200

    Es triangulo

---
+++ Tema 2/Estriangulo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Estriangulo {
+    public static boolean canbe(int x, int y, int z){
+        if (x>y+z || y>x+y || z>x+y){
+            return false ;
+        }else{
+            return true ;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in) ;
+
+        System.out.println("Give 3 sides of an triangle, and I'll tell if they can form a traingle:");
+        if (canbe(input.nextInt(), input.nextInt(), input.nextInt())){
+            System.out.println("They can form a triangle.");
+        }else{
+            System.out.println("They can't form a triangle.");
+        }
+        input.close() ;
+    }
+}
