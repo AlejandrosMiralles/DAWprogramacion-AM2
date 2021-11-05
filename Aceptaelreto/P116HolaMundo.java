4062cb0327f013b64296483db79125068c421d98
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-11-05 08:58:42 +0100

    Carpeta AceptaelReto subida+v P116

---
+++ Aceptaelreto/P116HolaMundo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P116HolaMundo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in) ;
+        
+        for (int i= input.nextInt(); i>0; --i){
+            System.out.println("Hola mundo.");
+        }
+    }
+}
