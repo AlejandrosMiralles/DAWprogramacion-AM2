f626ca0f2b0fa7b2ae961c387fb1e9021bd3c5af
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-11-05 12:06:47 +0100

    Ejercicio 10 funciona, creo

--- Controles/Control02/E10.java
+++ Controles/Control02/E10.java
@@ -1,5 +1,7 @@
 package Control02;
 
+import java.util.Scanner;
+
 //http://progra.usm.cl/apunte/ejercicios/1/huevos-a-la-copa.html
 
 public class E10 {
@@ -12,6 +14,17 @@ public class E10 {
         densidad = 1.038 ;
         capCalor = 3.7 ; 
         condCalor = 0.0054 ;
+        temEbullAgua = 100.0 + 273 ;
+        temYema = 343 ;
 
-    }
+        System.out.print("A cuántos grados está el huevo?(En Celsius): ");
+        temInic = input.nextDouble() +273;
+        input.close();
+
+        resultado = Math.pow(masa, 2/3)* Math.pow(densidad, 1/3)* capCalor ;
+        resultado = resultado/ (condCalor* Math.pow(Math.PI, 2)* Math.pow((4*Math.PI/3), 2/3)) ;
+        resultado = resultado * ((Math.log(0.76*(temInic-temEbullAgua)/(temYema-temEbullAgua)))/Math.log(Math.E)) ;
+
+        System.out.println("Tardará "+resultado+" segundos.");
+    }    
 }
