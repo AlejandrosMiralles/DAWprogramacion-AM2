bad3bb325e31e3a78233d46146f93d924ba0e811
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-11-10 10:37:44 +0100

    P616.V1.2 BIEN HECHO JOSDER ESTOY PUTS

--- Aceptaelreto/P616TrenyAndenV1.java
+++ Aceptaelreto/P616TrenyAndenV1.java
@@ -8,41 +8,27 @@ import java.util.Scanner;
 
 public class P616TrenyAndenV1 {
 
-    public static int callong(int repe,int[] posiciones, int puertas[], int long1){
-        if (repe!=0){
-            for (int x=0; x<posiciones.length; ++x){
-                if (repe>0){
-                    --repe;
-                }else{
-                    repe= puertas.length ;
-                }
+    public static int callong(int[] posiciones, int numpuertas, int long1){       
 
-                for (int z=0; z<puertas.length; ++z){
-                    if (puertas[z] == -1){
-                        puertas[z] = x;
-                    }
-                }
+        int long2, long3;
 
-                long1= callong(repe, posiciones, puertas, long1) ;
+        for (int j=0; j<posiciones.length-numpuertas; ++j){ //Miro todos los posibles lugares de las puertas
+            long2=0;
+
+            for (int i=0; i<posiciones.length; ++i){ //Calculo cuánto tarda en cada caso de las puertas
+                if (j>i){
+                    long3= (j-i)*posiciones[i];
+                    long2+=long3;
+                }else if(j+(numpuertas-1)<i){
+                    long3= (i-(j+numpuertas-1))*posiciones[i];
+                    long2+= long3;
+                }
             }
-         
-
-        }else{
-            int long2=0, long3;
-
-            for (int i=0; i<posiciones.length; ++i){
-                long3 = 100000000;
-                for (int j=0; j<puertas.length; ++j){
-                    if (long3>(Math.abs(puertas[j]-i)*posiciones[i])){
-                       long3= Math.abs(puertas[j]-i)*posiciones[i] ;
-                    }
-                }
-                long2 += long3 ;
-            }
-
+           
             if (long1>long2){
                 long1 = long2;
             }
+
         }
 
         return long1 ;
@@ -51,17 +37,16 @@ public class P616TrenyAndenV1 {
 
     public static void main(String[] args) {
         
-        int puertas=2, posicionesnum= 4, longitud = 10000000 ;
-        int posiciones[]= {1, 1, 1, 1}, puertasPosiciones[]= {-1, -1};
+        int puertas, posicionesnum, longitud = 10000000 ;
+        int posiciones[];
         Scanner input = new Scanner(System.in);
 
-        /*
+        
         puertas = input.nextInt() ;
         posicionesnum = input.nextInt();
 
 
         posiciones = new int[posicionesnum];
-        puertasPosiciones = new int[puertas];
 
 
         //Darle valores a la variable posiciones
@@ -69,17 +54,12 @@ public class P616TrenyAndenV1 {
             posiciones[i]= input.nextInt();
         }
 
-        for (int i=0; i<puertas; ++i){
-            puertasPosiciones[i]= -1;
-        }
-        */
-
         input.close();
 
         if (puertas>=posicionesnum){
             longitud = 0 ;
         }else{
-            System.out.println(callong(puertas, posiciones, puertasPosiciones, longitud));
+            System.out.println(callong(posiciones, puertas, longitud));
         }
     }
 }
