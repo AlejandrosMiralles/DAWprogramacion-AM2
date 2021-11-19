5f00c159214b99c7f50b957944d42515dfcb3c83
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-11-19 19:00:14 +0100

    DUPLICADOS PERFECTOOOOOOOOOO

--- Arrays/Duplicados.java
+++ Arrays/Duplicados.java
@@ -2,24 +2,26 @@ public class Duplicados {
     public static int[] repetidos(int[] numeros) {
 
         int[] repetidos = new int[0], repe;
-        int repeticiones=0;
+        int repeticiones, resto=0;
 
-        for (int i=0; i<numeros.length-repeticiones; ++i){
+        for (int i=0; i<numeros.length-resto; ++i){
             repe = new int[0];
+            repeticiones = 0;
 
-            for (int j=0; j<numeros.length-repeticiones; ++j){
-                if (numeros[i]==numeros[j] && i==j){
+            for (int j=0; j<numeros.length-resto; ++j){
+                if (numeros[i]==numeros[j] && i!=j){
                     repeticiones++;
                     repe= BibliotecaArrays.agregarEntero(repe,j);
                 }
             }
+            resto+= repeticiones;
 
             if (repe.length>0){
                 repetidos = BibliotecaArrays.agregarEntero(repetidos, numeros[i]);
             }
 
-            for (int posicion : repe){
-                numeros = BibliotecaArrays.alFinal(numeros, posicion);
+            for (int posicion=0; posicion<repe.length; ++posicion){
+                numeros = BibliotecaArrays.alFinal(numeros, repe[posicion]-posicion);
             }
 
         }
@@ -28,14 +30,21 @@ public class Duplicados {
     }
 
     public static void main(String[] args) {
-        int[] lista5= BibliotecaArrays.rellenaArray(10, -10, 10);
+        int[] lista5= BibliotecaArrays.rellenaArray(50, -10, 10);
+        
        
-        lista5= repetidos(lista5);
-
-
-        System.out.print("\n\n\nLista 5: ");
+        System.out.print("\n\n\nLista 5.0: ");
         for (int i=0; i<lista5.length; ++i){
             System.out.print(lista5[i]+"  ");
         }
+
+        lista5= repetidos(lista5);
+
+
+        System.out.print("\n\n\nLista 5.5: ");
+        for (int i=0; i<lista5.length; ++i){
+            System.out.print(lista5[i]+"  ");
+
+        }  
     }
 }
