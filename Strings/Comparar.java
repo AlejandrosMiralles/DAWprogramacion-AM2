4ad92db94de326fbbde37e1825c3c4bcb027519f
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-11-26 10:35:46 +0100

    Comienzo de los strings

---
+++ Strings/Comparar.java
@@ -0,0 +1,9 @@
+public class Comparar {
+    public static void main(String[] args) {
+        char data[] = {'a', 'b', 'c'};
+        String str = new String(data);
+        String str2 = "abc";
+        
+
+    }
+}
