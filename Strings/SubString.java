ea593c8f87cf2a0444afd1d847b04b0b7eaa8a10
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-11-29 09:29:08 +0100

    Substring

---
+++ Strings/SubString.java
@@ -0,0 +1,13 @@
+public class SubString {
+    public static void main(String[] args) {
+        String str = "sdvns", str2 = "sfsefiojsf", str3="snvns",str4="wsfno",
+        str5="wsfos", str6="somnsdovsdrvoivboisgoisriohn";
+
+        System.out.println(str.substring(1, 3));
+        System.out.println(str2.substring(4, 6));
+        System.out.println(str3.substring(0, 5));
+        System.out.println(str4.substring(3, 3));
+        System.out.println(str5.substring(0, 5));
+        System.out.println(str6.substring(15, 25));
+    }
+}
