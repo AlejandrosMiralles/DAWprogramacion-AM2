38e2fa93d07e4229ab09f171b64140a3e92a54c5
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-11-29 08:42:32 +0100

    Comparador de strings, pero sudando de las mayusculas y minusculas

---
+++ Strings/CompararIgnoreCase.java
@@ -0,0 +1,24 @@
+public class CompararIgnoreCase {
+    public static void comparador(String string1, String string2) {
+        int comparacion = string1.compareToIgnoreCase(string2);
+
+        if(comparacion>0){
+            System.out.println("La primera string ("+string1+") es mayor que la segunda ("+string2+").");
+        }else if(comparacion<0){
+            System.out.println("La primera string ("+string1+") es menor que la segunda ("+string2+").");
+        }else{
+            System.out.println("Las dos strings son iguales.");
+        }
+    }
+    public static void main(String[] args) {
+        char data[] = {'a', 'b', 'c'};
+        String str = new String(data);
+        String str2 = "abc", str3 = "ab", str4="abcd", str5="acd", str6= "ABC";
+
+        comparador(str, str2);
+        comparador(str, str3);
+        comparador(str, str4);
+        comparador(str, str5);
+        comparador(str, str6);
+    }
+}
