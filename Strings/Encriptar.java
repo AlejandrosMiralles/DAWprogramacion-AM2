13037d6af833f9e12392b15ba8079b8682562e0b
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-12-03 09:06:25 +0100

    Encriptar, incompletado

---
+++ Strings/Encriptar.java
@@ -0,0 +1,18 @@
+//a= 10 y z= 35. Las mayúsculas valen lo mismo que las minúsculas
+//Los digitos son iguales
+
+public class Encriptar {
+    public static char EncripDigitos(char digito, int clave) {
+        int utfOcho = Character.getNumericValue(digito);
+
+        if (utfOcho+clave>9){
+            return char 
+        }else{
+            return 
+        }
+    }
+
+    public static char EncripLetra(char letra, int clave) {
+        
+    }
+}
