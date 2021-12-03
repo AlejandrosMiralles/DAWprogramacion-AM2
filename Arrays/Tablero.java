c32ee5152aab290ea55c0c62d0174fa6dc97e9d2
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-12-03 10:35:26 +0100

    Programa para encriptar

--- Arrays/Tablero.java
+++ Arrays/Tablero.java
@@ -1,29 +1,60 @@
-public class Tablero {
-    public static int[][] rellenamatrices30000() {
-        //código copiado de #RellenaMatriz
+public class Tablero { 
+
+    public static int[] rellenaArray(int tamano, int min, int max) {
+        //Rellena un array nuevo de un tamaño con numeros aleatorios
+
+        int[] array= new int[tamano];
+
+		for (int i = 0; i < array.length; i++) {
+			int aleatorio = (int) (Math.random() * (max - min + 1) + min);
+			array[i] = aleatorio;
+		}
+        return array;
+    }
+
+    public static int[][] agregarMatriz(int[][] lista, int[] aagregar) {
+        //Agrega una fila en una matriz
+
+        int[][] lista2= new int[lista.length+1][];
+
+        System.arraycopy(lista, 0, lista2, 0, lista.length);
+        lista2[lista2.length-1]= aagregar;
+
+        return lista2;
+    }
+
+    public static int[][] creaTableros() {
+        //creo el tablero con 
 
         int[][] matriz = new int[0][8];
         int[] lista;
 
         for (int i = 0; i < 8; i++) {
-            lista= BibliotecaArrays.rellenaArray(8, 0, 3);
-            matriz= BibliotecaArrays.agregarMatriz(matriz, lista);
+            lista= rellenaArray(8, 0, 3);
+            matriz= agregarMatriz(matriz, lista);
         }
 
-        for (int i = 0; i < matriz.length; i++) {
-            BibliotecaArrays.escribeArray(matriz[i]);;
-        }
+        //Escribo el array para ver si el programa está bien hecho
+        /*for (int i = 0; i < matriz.length; i++) {  
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(" "+matriz[i][j]+" ");
+            }
+            System.out.println("");
+        }*/
+
+        
 
         return matriz;
     }
 
     public static void main(String[] args) {
-        int[][] matrizInt = rellenamatrices30000();
+        int[][] matrizInt = creaTableros();
         String[][] matrizStr = new String [9][9];
         String[] filainfo = {" ", "1","2","3","4","5","6","7","8"},
-                columnainfo = {" ", "A","B","C","D","E","F","G","H"};
-        
+        columnainfo = {" ", "A","B","C","D","E","F","G","H"};
 
+        
+        //Copia la natrizInt en la matrizStr, pero codificandola
         for (int i = 0; i < matrizInt.length; i++) {
             for (int j = 0; j < matrizInt[1].length; j++) {
                 if (matrizInt[i][j]==0){
@@ -38,11 +69,16 @@ public class Tablero {
             }
         }
     
+
+        //Añado la leyenda
         for (int i = 1; i < matrizStr.length; i++) {
             matrizStr[i][0]= columnainfo[i];
             matrizStr[0][i]= filainfo[i];
         }
+        matrizStr[0][0]= " ";
 
+
+        //Imprimo el tablero
         System.out.println("\n\n\n\n\n");
         for (int i = 0; i < matrizStr.length; i++) {
             for (int j = 0; j < matrizStr.length; j++) {
--- Strings/Encriptar.java
+++ Strings/Encriptar.java
@@ -1,18 +1,69 @@
 //a= 10 y z= 35. Las mayúsculas valen lo mismo que las minúsculas
 //Los digitos son iguales
 
+//import java.util.Scanner;
+
 public class Encriptar {
-    public static char EncripDigitos(char digito, int clave) {
+    //Hago dos funciones distintas porque debo diferenciar entre digitos y letras. 
+    //Me lo piden
+    public static char encripDigitos(char digito, int clave) {
         int utfOcho = Character.getNumericValue(digito);
 
         if (utfOcho+clave>9){
-            return char 
+            return Character.forDigit((utfOcho+clave)%10, 10);
         }else{
-            return 
+            return Character.forDigit(utfOcho+clave, 10);
         }
     }
 
-    public static char EncripLetra(char letra, int clave) {
-        
+    public static char encripLetra(char letra, int clave) {
+        int utfOcho = Character.getNumericValue(letra);
+        char codificado;
+        boolean highCase = false;
+
+        if (Character.isUpperCase(letra)){
+            highCase = true;
+        }
+
+        if (utfOcho+clave>35){
+            codificado =  Character.forDigit(((utfOcho+clave)%26)+10, 36);
+        }else{
+            codificado = Character.forDigit(utfOcho+clave, 36);
+        }
+
+        if (highCase){
+            codificado = Character.toUpperCase(codificado);
+        }
+
+        return codificado;
+    }
+
+    public static void main(String[] args) {
+        String cadena;
+       // Scanner input = new Scanner(System.in);
+        int clave = 55, utfOcho;
+        char encriptado;
+
+        cadena = "Una de las tecnicas de criptografia mas rudimentarias consiste en sustituir cada uno de los caracteres por otro situado n posiciones mas a la derecha del abecedario. Si n = 2, sustituiremos la 'a' por la 'c', la 'b' por la 'd', y asi sucesivamente. El problema que aparece con las ultimas n letras del alfabeto tiene facil solucion: en el ejemplo, la letra 'y' se sustituye por la 'a' y la 'z' por la 'b'. La sustitucion debe aplicarse a las letras minusculas y mayusculas y a los digitos. Las letras no deben incluir caracteres no ingleses (es decir sin ni acentos)";
+
+        /*
+        System.out.println("Dame una frase: ");
+        cadena = input.nextLine();
+        input.close();
+*/
+        for (int i = 0; i < cadena.length(); i++) {
+            utfOcho = Character.getNumericValue(cadena.charAt(i));
+
+            if (utfOcho<9 && utfOcho>=0){
+                encriptado = encripDigitos(cadena.charAt(i), clave);
+            }else if (utfOcho<36 && utfOcho>0){
+                encriptado= encripLetra(cadena.charAt(i), clave);
+            }else{
+                encriptado = cadena.charAt(i);
+            }
+
+            System.out.print(encriptado);
+        }
+        System.out.println("");
     }
 }
