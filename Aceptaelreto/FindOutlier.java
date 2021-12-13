5f28536ac53c933960b9449d1d57a509c4aff82d
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2021-12-13 12:31:55 +0100

    tonterías

--- Aceptaelreto/FindOutlier.java
+++ Aceptaelreto/FindOutlier.java
@@ -40,11 +40,5 @@ public class FindOutlier {
 
     public static void main(String[] args) {
     // https://www.codewars.com/kata/5526fc09a1bbd946250002dc/train/java
-    int[] exampleTest1 = {2,6,8,-10,3}; 
-     int[] exampleTest2 = {206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781}; 
-     int[] exampleTest3 = {Integer.MAX_VALUE, 0, 1};
-     assertEquals(3, FindOutlier.find(exampleTest1));
-     assertEquals(206847684, FindOutlier.find(exampleTest2));
-     assertEquals(0, FindOutlier.find(exampleTest3));
     }
 }
