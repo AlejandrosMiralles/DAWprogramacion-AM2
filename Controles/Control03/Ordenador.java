cb4302e60f409642d7cad949c8e67ee78cb0df77
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-02-04 10:44:20 +0100

    Ordenador casi finalizado

--- Controles/Control03/Ordenador.java
+++ Controles/Control03/Ordenador.java
@@ -3,12 +3,21 @@ package Control03;
 import java.util.ArrayList;
 import java.util.List;
 
+/** Una clase que describe a un ordenador
+ * @author Alejandro Miralles
+ * @version 2022.02.XX
+ */
 public class Ordenador {
-    private String formato;
-    private String color;
+    //Formato del ordenador. Puede ser o de sobremesa o pórtatil
+    private final String formato;
+    //Color del ordenador. No hay limitaciones en el color.
+    private final String color;
+    //Una objeto de la clase teclado. Todo ordenador sólo tiene un teclado
     private Teclado teclado;
+    //Los ordenadores se pueden conectar a otros ordenadores. Con este campo se guardan los ordenadores conectados
     private List<Ordenador> ordenadoresConectados;
 
+    
     public String getFormato() {return formato;}
     public String getColor() {return color;}
     public Teclado getTeclado() {return teclado;}
@@ -22,33 +31,46 @@ public class Ordenador {
     
     public void setTeclado(Teclado teclado) { this.teclado = teclado;}
   
-    
+    /** Constructor del ordenador
+     * @param formato Sólo puede ser o "sobremesa" o "portatil". Si se escribe otro, dará error
+     * @param Color No da errores. Se admite cualquier fadeena 
+     * @param Teclado Mira la clase teclado si quieres saber más. Avsio: puede dar errores 
+     * @exception ArithmeticalExcepction Si el formato o el teclado son inválidos, dará una excepción mátematica. 30 / 0
+     */
     public Ordenador(String formato, String color, Teclado teclado) {
         this.formato = checkFormato(formato);
         this.color = color;
         this.teclado = teclado;
         ordenadoresConectados = new ArrayList<>();
     }
+    /** Constructor default del ordenador
+     * @return un ordenador negro de sobremesa con un teclado construido con un constructor default, 
+     */
     public Ordenador() {
         this.formato = "Sobremesa";
         this.color = "Negro";
         this.teclado = new Teclado();
         ordenadoresConectados = new ArrayList<>();
     }
-
-    String checkFormato(String formato){
+    /**Se asegura que el formato del ordenador sea válido.
+     * @param formato Sólo puede ser o de: "sobremesa", "portatil" o "pórtatil". 
+     * @exception ArithmeticalExcepction Si el formato es erróneo, se hará un 30 / 0 y se causará un error
+     */
+    private String checkFormato(String formato){
         formato = formato.toLowerCase();
         if (formato.equals("sobremesa") || formato.equals("portatil") ||
             formato.equals("pórtatil")){ return formato;}
         return "" + 30/0;
     }
 
+
+    /** Conecta dos ordenadores entre sí. Se puede ver que ordenadores están conectados entre sí con getConectados */
     public void conectarOrdenador(Ordenador aConectar){ 
         ordenadoresConectados.add(aConectar);
         aConectar.seConecta(this);
     }
+    /** Método que se utiliza para conectar un ordenador a otro. Sólo lo utiliza el método conectarOrdernador */
     private void seConecta(Ordenador haciaOrdenador){
         ordenadoresConectados.add(haciaOrdenador);
-    }
-    
+    }   
 }
--- Controles/Control03/Teclado.java
+++ Controles/Control03/Teclado.java
@@ -1,27 +1,44 @@
 package Control03;
 
+/** Una clase que describe a un teclado
+ * @author Alejandro Miralles
+ * @version 2022.02.XX
+ */
 public class Teclado {
+    //Color del color
     private String color;
+    //Cantidad de teclas del teclado
     private final int NTeclas;
-    private final boolean conectividad;
+    //Si funciona con cable o es inálambrico. Es true si es inálambrico
+    private final boolean conectividadInalambrica;
+    //Idioma del teclado. Dependiendo de este, las teclas son distintas y con diferente posicionamiento
     private final String idioma;
+    //Formato del ordenador: sólo puede ser o compacto o extenddido
     private final String formato;
+    //Contador que cuenta la cantidad de teclados creados
     private static int contador = 0;
 
     public String getColor() { return color; }
    
     public int getNTeclas() { return NTeclas;}
    
-    public boolean isConectividad() { return conectividad;}
+    public boolean isConectividad() { return conectividadInalambrica;}
 
     public String getIdioma() {return idioma;}
  
     public String getFormato() {return formato;}
 
+    /** Constructor del teclado
+     * @param formato Sólo puede ser o "compacto" o "extendido". Si se escribe otro, dará error
+     * @param Color No da errores. Se admite cualquier fadeena 
+     * @param nTeclas Define la cantidad de teclas que tiene un teclado dado
+     *  
+     * @exception ArithmeticalExcepction Si el formato o el teclado son inválidos, dará una excepción mátematica. 30 / 0
+     */
     public Teclado(String color, int nTeclas, boolean conectividad, String idioma, String formato) {
         this.color = color;
         NTeclas = checkTeclas(nTeclas);
-        this.conectividad = conectividad;
+        this.conectividadInalambrica = conectividad;
         this.idioma = checkIdioma(idioma);
         this.formato = checkFormato(formato);
 
@@ -31,7 +48,7 @@ public class Teclado {
     public Teclado() {
         this.color = "Negro";
         NTeclas = 100;
-        this.conectividad = false;
+        this.conectividadInalambrica = false;
         this.idioma = "es";
         this.formato = "Extendido";
 
