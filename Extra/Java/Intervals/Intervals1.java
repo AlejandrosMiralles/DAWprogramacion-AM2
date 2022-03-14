0443bf11916b55c92cdab9fb99c0bf87d050b393
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-03-14 19:05:49 +0100

    Intervales casi terminado: falta hacer una limpieza de fallos

---
+++ Extra/Java/Intervals/Intervals1.java
@@ -0,0 +1,65 @@
+package Intervals;
+
+//https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/java
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class Intervals1 {
+
+    public static int sumIntervals(int[][] intervals) {
+        int[][] finalIntervals = removeOverlapping(intervals);
+
+        int finalLength = 0;
+        for (int[] interval : finalIntervals) {
+            finalLength+= (interval[1] - interval[0]);
+        }
+
+        return finalLength;
+    }
+
+    private static int[][] removeOverlapping(int[][] intervals){
+        ArrayList<Integer[]> resultList = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            addInterval(resultList, interval);
+        }
+
+        int[][] result = new int[resultList.size()][2];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = new int[]{ resultList.get(i)[0], resultList.get(i)[1]};
+        }
+
+        return result;
+    }
+
+    private static void addInterval(ArrayList<Integer[]> list, int[] interval){
+        Iterator<Integer[]> iteranator = list.iterator();
+        Integer[] intervalToAdd = new Integer[]{ interval[0], interval[1]};
+
+        while(iteranator.hasNext()){
+            Integer[] intervalMember = iteranator.next();
+
+            if (EstaDentroDe(intervalToAdd, intervalMember)){ 
+                return;
+            }else if (EstaDentroDe(intervalMember, intervalToAdd)){
+                list.remove(intervalMember);
+            }else if (intervalToAdd[0] <= intervalMember[0] && intervalToAdd[1] > intervalMember[0]){ 
+                list.remove(intervalMember);
+                intervalToAdd = new Integer[]{intervalToAdd[0], intervalMember[1]};
+            }else if (intervalToAdd[0] < intervalMember[1] && intervalToAdd[1] >= intervalMember[1]){
+                list.remove(intervalMember);
+                intervalToAdd = new Integer[]{ intervalMember[0], intervalToAdd[1]};
+            }
+        }
+
+        list.add(intervalToAdd);
+    }
+
+    private static boolean EstaDentroDe(Integer[] interval1, Integer[] interval2) {
+        return interval2[0] <= interval1[0] && interval2[1] >= interval1[1];
+    }
+
+}
