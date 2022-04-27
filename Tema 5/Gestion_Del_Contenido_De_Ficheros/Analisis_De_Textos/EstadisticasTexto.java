c4ce76157cfb404781697f64e35e19ace68b718e
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-04-27 09:46:25 +0200

    EstadisticasTexto

--- Tema 5/Gestion_Del_Contenido_De_Ficheros/Analisis_De_Textos/EstadisticasTexto.java
+++ Tema 5/Gestion_Del_Contenido_De_Ficheros/Analisis_De_Textos/EstadisticasTexto.java
@@ -13,15 +13,18 @@ public class EstadisticasTexto {
     private int espacios = 0;
     private int tabuladores = 0;
     private int caracteresDesconocidos = 0;
-    private int lineas = 0;
+    private int lineas = 1; //Incluso si un fichero está vacío, se contará como que tiene una linea
 
 
-    EstadisticasTexto(String rutaArchivo) {
+    EstadisticasTexto(String rutaArchivo) throws FileNotFoundException, IOException{
         this.archivo = new File(rutaArchivo);
     
+        if (archivo.exists()){
+            analizarTexto();
+        }
     }
 
-    void analizarTexto() throws FileNotFoundException, IOException{
+    private void analizarTexto() throws FileNotFoundException, IOException{
 
         FileReader lector = new FileReader(archivo);
     
@@ -29,9 +32,14 @@ public class EstadisticasTexto {
 
         caracter = (char) lector.read();
         while(caracter != (char)-1){
-            switch(caracter){
-                case 
-            }
+            String aComprobar = "" + Character.toLowerCase(caracter);
+
+            if (aComprobar.matches("[aeiou]")){ vocales++;}
+            else if(aComprobar.matches("[a-z]")){ consonantes++;}
+            else if(aComprobar.equals(" ")){ espacios++;}
+            else if(aComprobar.equals("\n")){ lineas++;}
+            else if(aComprobar.equals("\t")){ tabuladores++;}
+            else{ caracteresDesconocidos++;}
 
             caracter = (char) lector.read();
         }
@@ -39,5 +47,63 @@ public class EstadisticasTexto {
         lector.close();
     }
 
+    public void setFile(String rutaArchivo) throws FileNotFoundException, IOException{
+        this.archivo = new File(rutaArchivo);
+
+        reiniciarCamposDelObjeto();
+    
+        if (archivo.exists()){
+            analizarTexto();
+        }
+    }
+
+    private void reiniciarCamposDelObjeto() {
+        vocales = 0;
+        consonantes = 0;
+        espacios = 0;
+        tabuladores = 0;
+        caracteresDesconocidos = 0;
+        lineas = 1;
+    }
+
+    public void imprimirAnalisisDelFichero(){
+        int letras = vocales + consonantes;
+        int caracteresTotales = letras + espacios + tabuladores + caracteresDesconocidos;
+
+        if (caracteresTotales == 0){
+            System.out.printf("Ficher: %s \nLineas: %-4d\nLetras: %-4d\nVocale: %-4d %.2f%%\n" + 
+              "Conson: %-4d %.2f%%\nEspaci: %-4d %.2f%%\nTabula: %-4d %.2f%%\n",
+                    archivo.getAbsolutePath(),
+                    lineas, 
+                    letras,
+                    vocales, 0.0,
+                    consonantes, 0.0, 
+                    espacios, 0.0, 
+                    tabuladores, 0.0);
+        }else{
+            System.out.printf("%-15s %s \n%-15s %-7d\n%-15s %-7d\n%-15s %-7d %.2f%%\n" + 
+                "%-15s %-7d %.2f%%\n%-15s %-7d %.2f%%\n%-15s %-7d %.2f%%\n",
+                        "Fichero:", archivo.getAbsolutePath(),
+                        "Lineas:", lineas, 
+                        "Letras:", letras,
+                        "Vocales:", vocales, (float) vocales/caracteresTotales * 100,
+                        "Consonantes:", consonantes, (float) consonantes/caracteresTotales*100, 
+                        "Espacios:", espacios, (float) espacios/caracteresTotales *100, 
+                        "Tabuladores:", tabuladores, (float) tabuladores/caracteresTotales *100);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            String archivo = "Tema 5/Gestion_Del_Contenido_De_Ficheros/Analisis_De_Textos/" +
+                "readme.md";
+
+            EstadisticasTexto tester = new EstadisticasTexto(archivo);
+
+            tester.imprimirAnalisisDelFichero();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
 
 }
--- Tema 5/Gestion_Del_Contenido_De_Ficheros/Analisis_De_Textos/readme.md
+++ Tema 5/Gestion_Del_Contenido_De_Ficheros/Analisis_De_Textos/readme.md
@@ -1,6 +1,5 @@
 Enunciado:
     Estadisticas Texto. Crea un programa que analice un fichero de texto plano y que muestre las siguientes estadisticas:
 
-    -Número de letras, palabras y líneas
+    -Número de letras,  palabras y líneas
     -Porcentaje de vocales, porcentaje de consonantes, de espacios y tabuladores
-    
